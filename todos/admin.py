7ed6c40fbd1acdf28ab24039cc969b8c7679b273
Ryan <ryan.abolfazl@gmail.com>
2024-07-12 10:15:25 +0330

    final traditional django section

--- todos/admin.py
+++ todos/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from todos.models import Todo
+
+
 # Register your models here.
+
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'body',
+    )
+
+
+admin.site.register(Todo, TodoAdmin)
--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
-
+from .models import Todo
 # Create your tests here.
+
+class TodoModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.todo = Todo.objects.create(
+            title='Test title',
+            body='test body',
+        )
+
+    def test_model_content(self):
+        self.assertEqual(self.todo.title, 'Test title')
+        self.assertEqual(self.todo.body, 'test body')
+        self.assertEqual(str(self.todo), 'Test title')
